ddfb7701d8e9ab7fff5e2cbd4e4e1233a997abf9
Lena Kilian <43342015+lena-kilian@users.noreply.github.com>
2023-09-25 15:16:45 +0100

    Update import_data.py

--- src/import_data.py
+++ src/import_data.py
@@ -5,3 +5,21 @@ Created on Mon Sep 25 14:31:51 2023
 @author: geolki
 """
 
+# use pymrio https://pymrio.readthedocs.io/en/latest/notebooks/autodownload.html
+
+import pymrio
+import os
+
+
+mrio_data_path = 'O:/geolki/data/raw/MRIOs/'
+
+db = ['EXIO3']
+
+for item in db:
+    newpath = mrio_data_path + item
+    if not os.path.exists(newpath):
+        os.makedirs(newpath)
+        
+
+data = pymrio.download_exiobase3(storage_folder=mrio_data_path + 'EXIO3/', system="pxp", years=[2011, 2012])
+
