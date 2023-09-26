2e75ce6f3161a74422f0c095fa2f6d69121fe1fe
Lena Kilian <43342015+lena-kilian@users.noreply.github.com>
2023-09-26 15:00:50 +0100

    Update import_data.py

--- src/import_data.py
+++ src/import_data.py
@@ -13,13 +13,12 @@ import os
 
 mrio_data_path = 'O:/geolki/data/raw/MRIOs/'
 
-db = ['EXIO3']
+db = ['Exiobase3', 'OECD']
 
 for item in db:
     newpath = mrio_data_path + item
     if not os.path.exists(newpath):
         os.makedirs(newpath)
         
-
-data = pymrio.download_exiobase3(storage_folder=mrio_data_path + 'EXIO3/', system="pxp", years=[2011, 2012])
-
+exio = pymrio.download_exiobase3(storage_folder=mrio_data_path + 'Exiobase3/', system="pxp", years=[2011])
+oecd = pymrio.download_oecd(storage_folder=mrio_data_path + 'OECD/', version="v2016", years=[2003, 2008])
