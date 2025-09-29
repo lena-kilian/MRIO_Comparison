23a774c8448e7ca6cb1627883ae0224be762a57e
Lena Kilian <43342015+lena-kilian@users.noreply.github.com>
2025-09-29 08:38:25 +0100

    Update calculate_ghg_extension_exio394.py

--- src/calculate_ghg_extension_exio394.py
+++ src/calculate_ghg_extension_exio394.py
@@ -5,3 +5,21 @@ Created on Mon Sep 29 07:41:09 2025
 @author: geolki
 """
 
+import pandas as pd
+
+years = range(2010, 2022)
+
+# import files
+# read conversion file
+co2e_conversion = pd.read_csv('O:/EXIOBASE/EXIOBASE310f_CCf_list.csv')
+# read extensions
+extensions = {}
+for year in years:
+    extensions[year] = pd.read_csv('O:/EXIOBASE/3.9.6 ixi/' + str(year) + '/air_emissions/F.txt')
+
+
+# filter for AR5
+co2e_conversion = co2e_conversion.loc[co2e_conversion['impact'] == 'GHG emissions AR5 (GWP100)|kg CO2 eq.||GWP100 (IPCC, 2010)']
+
+
+
